71f821385216d16d658a993535455b0556793279
TEZOdeer <124118299+TEZOdeer@users.noreply.github.com>
2023-02-13 11:05:06 +0900

    Тест теста

---
+++ p0051layoutfiles/src/main/java/com/example/p0051_layoutfiles/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p0051_layoutfiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscreen);
+    }
+}
---
+++ p0061layouts/src/main/java/ru/androidlessons/p0061layouts/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.androidlessons.p0061layouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
