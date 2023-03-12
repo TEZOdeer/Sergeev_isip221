d3b894c05c1403aa1c6265f712f4b7b352083f19
TEZOdeer <124118299+TEZOdeer@users.noreply.github.com>
2023-03-12 12:57:34 +0900

    12.03.2023 23:08 Guess_The_Number v0.2

--- guess_the_number/src/main/java/ru/sergeev/guess_the_number/MainActivity.java
+++ guess_the_number/src/main/java/ru/sergeev/guess_the_number/MainActivity.java
@@ -20,9 +20,14 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     TextView tvinfo;
+    TextView lifes_count;
     EditText etinput;
     Button Btn;
     Button Restart;
+    int guess;
+    int value;
+    boolean gameFinished;
+    int lifes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,44 +38,74 @@ public class MainActivity extends AppCompatActivity {
         etinput = (EditText) findViewById(R.id.etinput);
         Btn = (Button) findViewById(R.id.Btn);
         Restart = (Button) findViewById(R.id.Restart);
+        lifes_count = (TextView) findViewById(R.id.lifes_count);
         Restart.setEnabled(false);
+        gameFinished = false;
+        guess = (int)(Math.random()*10);
+        lifes = 3;
+        value=-1;
+        lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
     }
 
-
-
     public void BtnClick(View v) {
-        int guess = (int) (Math.random() * 10);
-        boolean gameFinished = false;
-        if (!gameFinished) {
-            //int value= Integer.parseInt(etinput.getText().toString());
-            int value = Integer.parseInt(etinput.getText().toString());
+        lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
+        try {
+            value = Integer.parseInt(etinput.getText().toString());
+        } catch (NumberFormatException exception) {
+            tvinfo.setText(R.string.error);
+        }
 
-            if (TextUtils.isEmpty(etinput.getText().toString())) {
-                return;
-            }
-            else if (value > 100 | value < 1)
+
+        if (!gameFinished) {
+            if (value > 10 | value < 0) {
                 tvinfo.setText(R.string.correct);
-            else if (value > guess)
+                lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
+            }
+            else if (value > guess) {
                 tvinfo.setText(R.string.ahead);
-            else if (value < guess)
+                lifes --;
+                lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
+            }
+            else if (value < guess) {
                 tvinfo.setText(R.string.behind);
+                lifes --;
+                lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
+            }
             else if (value == guess) {
                 tvinfo.setText(R.string.hit);
                 gameFinished = true;
             }
+            else if (value < 0) {
+                tvinfo.setText(R.string.correct);
+                lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
+            }
+        }
+        else {
+            guess = (int) (Math.random() * 10);
+            Btn.setText(R.string.input_value);
+            tvinfo.setText(R.string.try_to_guess);
+            gameFinished = false;
+            lifes = 3;
+            lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
         }
         if (gameFinished==true) {
             Btn.setEnabled(false);
             Restart.setEnabled(true);
             etinput.setEnabled(false);
+            lifes = 3;
+            lifes_count.setText(String.valueOf(getResources().getString(R.string.left_lifes) + lifes));
+        }
+        if (lifes==0) {
+            tvinfo.setText(R.string.over);
+            Btn.setEnabled(false);
+            Restart.setEnabled(true);
+            etinput.setEnabled(false);
+            lifes_count.setText(String.valueOf(guess));
         }
 
     }
     public void Exit (View v) {
         finishAffinity();
-    }
-    protected boolean isEmpty(EditText etinput) {
-        return (etinput.getText().toString().equals(""));
     }
     public void again (View v) {
         Intent Main = new Intent(this, MainActivity.class);
--- p0111_resvalues/src/main/java/ru/startandroid/sergeev/p0111_resvalues/MainActivity.java
+++ p0111_resvalues/src/main/java/ru/startandroid/sergeev/p0111_resvalues/MainActivity.java
@@ -8,18 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
     /** Called when the activity is first created. */
-    @Override
-    public <TextView> void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
-        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
-        Button btnBottom = (Button) findViewById(R.id.btnBottom);
 
-        llBottom.setBackgroundResource(R.color.llBottomColor);
-        tvBottom.setText(R.string.tvBottomText);
-        btnBottom.setText(R.string.btnBottomText);
-
-    }
 }
